4ab3838d6b35da271e8acc8d66069acb6533f904
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2025-04-21 22:27:54 -0300

    chore: cors

--- src/app.ts
+++ src/app.ts
@@ -1,93 +1,94 @@
 //Importa as váriaveis ambiente
-import 'dotenv/config'
+import "dotenv/config";
+
+import cors from "cors";
 
 // Importa o módulo que facilita a criação de erros HTTP (como 404, 500, etc)
-import createError from 'http-errors'
+import createError from "http-errors";
 
 // Importa o Express e os tipos do TypeScript para Request, Response e NextFunction
-import express, { Request, Response, NextFunction } from 'express'
+import express, { Request, Response, NextFunction } from "express";
 
 // Módulo para trabalhar com caminhos de arquivos/diretórios
-import path from 'path'
+import path from "path";
 
 // Middleware para parsear cookies
-import cookieParser from 'cookie-parser'
+import cookieParser from "cookie-parser";
 
 // Middleware para fazer logs de requisições (como método, status, tempo)
-import logger from 'morgan'
+import logger from "morgan";
 
 // Importa os roteadores definidos nos arquivos de rotas
-import indexRouter from './routes/index';
-
+import indexRouter from "./routes/index";
 
 //TSOA
-import { RegisterRoutes } from './routes/routes'
-import { ValidateError } from 'tsoa';
+import { RegisterRoutes } from "./routes/routes";
+import { ValidateError } from "tsoa";
 
 //Error do express
-import { ErrorRequestHandler } from 'express';
-
+import { ErrorRequestHandler } from "express";
 
 // Config Swagger para as rotas
-import swaggerUi from 'swagger-ui-express'
-import swaggerDocument from '../src/docs/swagger.json';
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../src/docs/swagger.json";
 
 // Cria a instância da aplicação Express
-const app = express()
+const app = express();
 
 // -------- CONFIGURAÇÕES DO SERVIDOR --------
 
 // Define a pasta onde estão os arquivos de views/templates (ex: arquivos Jade/Pug)
-app.set('views', path.join(__dirname, 'views'))
+app.set("views", path.join(__dirname, "views"));
 
 // Define o motor de visualização (template engine) usado para renderizar as views
-app.set('view engine', 'jade')
+app.set("view engine", "jade");
 
 // -------- MIDDLEWARES GLOBAIS --------
 
 // Ativa o middleware de log das requisições
-app.use(logger('dev'))
+app.use(logger("dev"));
 
 // Permite que a aplicação aceite JSON no corpo da requisição
-app.use(express.json())
+app.use(express.json());
 
 // Permite que a aplicação aceite dados codificados na URL (formulários)
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
 // Permite que cookies sejam lidos e interpretados
-app.use(cookieParser())
+app.use(cookieParser());
 
 // Define a pasta pública para arquivos estáticos (como CSS, imagens, etc)
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
+
+// Ativa o CORS para todas as origens (pode ser configurado depois)
+app.use(cors());
 
 // -------- ROTAS --------
 
-//Swagger 
+//Swagger
 
 function setupSwagger(app: express.Application) {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 }
 
-setupSwagger(app)
+setupSwagger(app);
 
-RegisterRoutes(app)
-
+RegisterRoutes(app);
 
 // Rota raiz ("/") será tratada pelo indexRouter
 // app.use('/', indexRouter)
 
 //Coloquei para ir para api-docs logo quando abre por enquanto(swagger)
-app.get('/', (_req, res) => {
-  res.redirect('/api-docs');
+app.get("/", (_req, res) => {
+  res.redirect("/api-docs");
 });
-
 
 // -------- TRATAMENTO DE ERRO 404 --------
 
 // Quando nenhuma rota acima for atendida, cai aqui e gera um erro 404
 app.use((req: Request, res: Response, next: NextFunction) => {
-  next(createError(404)) // cria erro e encaminha para o handler de erro
-})
+  next(createError(404)); // cria erro e encaminha para o handler de erro
+});
 
 // -------- TRATAMENTO DE ERROS GERAIS --------
 
@@ -97,11 +98,9 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   para ValidatorError do TSOA em alguns casos.)
 
 */
-import { ErrorHandlerMiddleware } from './middlewares/ErrorHandlerMiddleware'
+import { ErrorHandlerMiddleware } from "./middlewares/ErrorHandlerMiddleware";
 
-app.use(ErrorHandlerMiddleware.handle)
-
-
+app.use(ErrorHandlerMiddleware.handle);
 
 // Exporta o app para ser usado por outro arquivo (ex: server.ts)
-export default app
+export default app;
